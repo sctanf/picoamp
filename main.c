76d741a375c623ee2f6575d9d38323c028db2b4f
sctanf <36978460+sctanf@users.noreply.github.com>
2024-08-07 18:07:18 -0500

    use bootsel pin as reboot key
    https://github.com/jasongaunt/rp2040-bootsel-reboot-example/
    
    also, if usb fails for some reason (I don't know why??), watchdog additionally works to restart the device

--- main.c
+++ main.c
@@ -1017,6 +1017,48 @@ static void __not_in_flash_func(core1_worker)() {
     audio_i2s_set_enabled(true);
 }
 
+// https://github.com/jasongaunt/rp2040-bootsel-reboot-example/
+#include "hardware/structs/ioqspi.h"
+#include "hardware/watchdog.h"
+
+bool __no_inline_not_in_flash_func(get_bootsel_button)() {
+	const uint CS_PIN_INDEX = 1;
+
+	// Must disable interrupts, as interrupt handlers may be in flash, and we
+	// are about to temporarily disable flash access!
+	uint32_t flags = save_and_disable_interrupts();
+
+	// Set chip select to Hi-Z
+	hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
+		GPIO_OVERRIDE_LOW << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
+		IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
+
+	// Note we can't call into any sleep functions in flash right now
+	for (volatile int i = 0; i < 1000; ++i);
+
+	// The HI GPIO registers in SIO can observe and control the 6 QSPI pins.
+	// Note the button pulls the pin *low* when pressed.
+	bool button_state = !(sio_hw->gpio_hi_in & (1u << CS_PIN_INDEX));
+
+	// Need to restore the state of chip select, else we are going to have a
+	// bad time when we return to code in flash!
+	hw_write_masked(&ioqspi_hw->io[CS_PIN_INDEX].ctrl,
+		GPIO_OVERRIDE_NORMAL << IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_LSB,
+		IO_QSPI_GPIO_QSPI_SS_CTRL_OEOVER_BITS);
+
+	restore_interrupts(flags);
+
+	return button_state;
+}
+
+void arm_watchdog() {
+	watchdog_enable(500, 1);
+}
+
+void update_watchdog() {
+	watchdog_update();
+}
+
 int main(void) {
     set_sys_clock_khz(270000, true); // so i can fit more filters
 
@@ -1047,5 +1089,11 @@ audioi2sconstuff(&bufring1);
 
     multicore_launch_core1(core1_worker);
     // MSD is irq driven
-    while (1) __wfi();
+    arm_watchdog();
+    while (1) {
+        __wfi();
+        if (get_bootsel_button())
+            while(1);
+        update_watchdog();
+    }
 }
